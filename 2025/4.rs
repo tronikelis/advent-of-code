2cc860ed7d8117a125528936ae400e9d4793ba8d
Tronikel <contactdonatas@gmail.com>
2025-12-04 16:56:42 +0200

    feat: d4 p2

--- 2025/4.rs
+++ 2025/4.rs
@@ -35,47 +35,59 @@ impl Grid {
     fn access_count(&mut self) -> usize {
         let mut count = 0;
 
-        for y in 0..self.0.len() {
-            for x in 0..self.0[y].len() {
-                let Pos::Roll = self.0[y][x] else {
-                    print!(".");
-                    continue;
-                };
+        // remove this while(true) loop for part 1
+        loop {
+            let prev_count = count;
+            for y in 0..self.0.len() {
+                for x in 0..self.0[y].len() {
+                    let Pos::Roll = self.0[y][x] else {
+                        print!(".");
+                        continue;
+                    };
 
-                let mut rolls = 0;
+                    let mut rolls = 0;
 
-                // check 3 adj positions top
-                let mut top_row = (y as isize) - 1;
-                if top_row >= 0 {
-                    self.check_3_adj(top_row as usize, x, &mut rolls);
+                    // check 3 adj positions top
+                    let mut top_row = (y as isize) - 1;
+                    if top_row >= 0 {
+                        self.check_3_adj(top_row as usize, x, &mut rolls);
+                    }
+
+                    // check left
+                    let left_row = (x as isize) - 1;
+                    if left_row >= 0 {
+                        Self::check_pos(self.0[y][left_row as usize], &mut rolls);
+                    }
+
+                    // check right
+                    let right_row = x + 1;
+                    if right_row < self.0[y].len() {
+                        Self::check_pos(self.0[y][right_row as usize], &mut rolls);
+                    }
+
+                    // check 3 adj positions bottom
+                    let mut bottom_row = y + 1;
+                    if bottom_row < self.0.len() {
+                        self.check_3_adj(bottom_row, x, &mut rolls);
+                    }
+
+                    if rolls < 4 {
+                        // remove this for part 1
+                        self.0[y][x] = Pos::Empty;
+
+                        count += 1;
+                        print!("x");
+                    } else {
+                        print!("@");
+                    }
                 }
-
-                // check left
-                let left_row = (x as isize) - 1;
-                if left_row >= 0 {
-                    Self::check_pos(self.0[y][left_row as usize], &mut rolls);
-                }
-
-                // check right
-                let right_row = x + 1;
-                if right_row < self.0[y].len() {
-                    Self::check_pos(self.0[y][right_row as usize], &mut rolls);
-                }
-
-                // check 3 adj positions bottom
-                let mut bottom_row = y + 1;
-                if bottom_row < self.0.len() {
-                    self.check_3_adj(bottom_row, x, &mut rolls);
-                }
-
-                if rolls < 4 {
-                    count += 1;
-                    print!("x");
-                } else {
-                    print!("@");
-                }
+                println!();
             }
             println!();
+            println!();
+            if prev_count == count {
+                break;
+            }
         }
 
         count
